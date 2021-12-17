90d35770e3c00a232cb3e2f420821fc6fd6de6a6
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-12-17 01:27:10 +0100

    Try to apply GNOME guidelines to ApiResolver

--- gum/gumapiresolver.c
+++ gum/gumapiresolver.c
@@ -4,6 +4,34 @@
  * Licence: wxWindows Library Licence, Version 3.1
  */
 
+/**
+ * ApiResolver:
+ *
+ * Resolves query strings into memory locations where matching functions reside.
+ *
+ * ## Using `ApiResolver`
+ *
+ * ```c
+ * void
+ * start (void)
+ * {
+ *   g_autoptr(ApiResolver) resolver = gum_api_resolver_make ("module");
+ *
+ *   gum_api_resolver_enumerate_matches (resolver, "exports:libc.so!open*",
+ *                                       instrument_libc_open, NULL, NULL);
+ * }
+ *
+ * static gboolean
+ * instrument_libc_open (const GumApiDetails *details,
+ *                       gpointer user_data)
+ * {
+ *   g_print ("Found %s at %" G_GINT64_MODIFIER "x\n",
+ *            details->name,
+ *            details->address);
+ * }
+ * ```
+ */
+
 #include "gumapiresolver.h"
 
 #include "gummoduleapiresolver.h"
@@ -22,24 +50,24 @@ gum_api_resolver_default_init (GumApiResolverInterface * iface)
 
 /**
  * gum_api_resolver_make:
+ * @type: the resolver type to make
  *
- * Creates a #GumApiResolver of the given @type, allowing you to quickly find
+ * Makes an API resolver of the given @type, allowing you to quickly find
  * functions by name, with globs permitted. Precisely which resolvers are
  * available depends on the current platform and runtimes loaded in the current
- * process. As of the time of writing, the available resolvers are:
+ * process. Available resolvers are currently:
  *
- * - `module`: Resolves exported and imported functions of shared libraries
- *   currently loaded. Always available.
- * - `objc`: Resolves Objective-C methods of classes currently loaded. Available
- *   on macOS and iOS in processes that have the Objective-C runtime loaded.
+ *  - `module`: Resolves exported and imported functions of shared libraries
+ *    currently loaded. Always available.
+ *  - `objc`: Resolves Objective-C methods of classes currently loaded. Available
+ *    on macOS and iOS in processes that have the Objective-C runtime loaded.
  *
  * The resolver will load the minimum amount of data required on creation, and
- * lazy-load the rest depending on the queries it receives. It is thus
- * recommended to use the same instance for a batch of queries, but recreate it
- * for future batches to avoid looking at stale data.
+ * lazy-load the rest depending on the queries it receives. You should use the
+ * same instance for a batch of queries, but recreate it for future batches to
+ * avoid looking at stale data.
  *
- * Returns: (nullable) (transfer full): a newly created #GumApiResolver,
- * or %NULL if the specified @type is not available
+ * Returns: (nullable) (transfer full): the newly created resolver instance
  */
 GumApiResolver *
 gum_api_resolver_make (const gchar * type)
@@ -57,11 +85,11 @@ gum_api_resolver_make (const gchar * type)
 
 /**
  * gum_api_resolver_enumerate_matches:
- * @self: a #GumApiResolver
- * @query: resolver-specific query string
- * @func: (scope call): function called with #GumApiDetails
- * @user_data: data to pass to @func
- * @error: return location for an error, or %NULL to ignore
+ * @self: a resolver
+ * @query: the query to perform
+ * @func: (scope call): the function called with each match
+ * @user_data: (nullable): the data to pass to @func
+ * @error: (nullable): return location for a #GError
  *
  * Performs the resolver-specific @query, optionally suffixed with `/i` to
  * perform case-insensitive matching. Calls @func with each match found.
